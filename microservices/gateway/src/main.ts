8aecd59f89bf59a7d5e137eaa844ab81e681a295
philip <philipatdev@gmail.com>
2022-12-23 13:24:00 +0200

    refactored scaffolding to support microservice architecture

--- microservices/gateway/src/main.ts
+++ microservices/gateway/src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { MediaModule } from './api';
+import {Transport} from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(MediaModule);
+
+  app.connectMicroservice({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://localhost:5000'],
+      queue: 'media:process',
+      queueOptions: {
+        durable: false
+      },
+    },
+  })
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
