354a5613e653a842fbdcbdcf24af28c4084d0ee0
philip <philipatdev@gmail.com>
2022-12-31 01:47:24 +0200

    feat:created shared error-message enum

---
+++ microservices/media-processor/src/media-processor/error-message.enum.ts
@@ -0,0 +1,6 @@
+enum ErrorMessage {
+	CONTENT_TYPE_IS_NOT_SUPPORTED = 'content-type provided by the link is not supported',
+	CONTENT_SIZE_IS_TOO_LARGE = 'content size is too large',
+}
+
+export { ErrorMessage };
