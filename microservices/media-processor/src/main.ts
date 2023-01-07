9234c9a909605956034a972621a9943ed2383496
philip <philipatdev@gmail.com>
2023-01-07 14:02:07 +0200

    feat:removed nest default rmq connection

--- microservices/media-processor/src/main.ts
+++ microservices/media-processor/src/main.ts
@@ -1,26 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
-import {
-  RABBIT_MQ_QUEUE_NAME,
-  RABBIT_MQ_HOST,
-  RABBIT_MQ_PORT,
-  RABBIT_MQ_USER, RABBIT_MQ_PASS
-} from '../config';
+import { MicroserviceOptions } from '@nestjs/microservices';
 import { MediaProcessorModule } from './media-processor/media-processor.module';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     MediaProcessorModule,
-    {
-      transport: Transport.RMQ,
-      options: {
-        urls: [`amqp://${RABBIT_MQ_USER}:${RABBIT_MQ_PASS}@${RABBIT_MQ_HOST}:${RABBIT_MQ_PORT}`],
-        queue: RABBIT_MQ_QUEUE_NAME,
-        queueOptions: {
-          durable: false
-        },
-      },
-    },
   );
   await app.listen();
 }
