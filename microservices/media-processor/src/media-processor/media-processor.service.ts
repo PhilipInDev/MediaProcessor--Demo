5d19e0f6a297451397ceaa04e11bd5ecd6c0e4a4
philip <philipatdev@gmail.com>
2023-01-07 14:07:15 +0200

    feat:added file uploading,validation and metadata retrieving logic to media processor service

--- microservices/media-processor/src/media-processor/media-processor.service.ts
+++ microservices/media-processor/src/media-processor/media-processor.service.ts
@@ -1,10 +1,74 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { RpcException } from '@nestjs/microservices';
+import { AmqpConnection } from '@golevelup/nestjs-rabbitmq';
+import { ErrorMessage } from './error-message.enum';
+import { FileType } from './file-types.enum';
+import { supportedMediaTypesConfig } from './supported-media-types.config';
+import { MPHelpers } from './media-processor.helpers';
+import { MAX_FILE_SIZE_MB } from '../../config';
 
 @Injectable()
 class MediaProcessorService {
-	processFile(): string {
-		throw new RpcException('Exception from media-processor service');
+	constructor(
+		private readonly amqpConnection: AmqpConnection
+	) {}
+
+	private MAX_FILE_SIZE_MB = Number(MAX_FILE_SIZE_MB);
+
+	public async scanContentForExceptions(fileUrl: string) {
+		const { headers: metaHeaders } = await fetch(fileUrl, { method: 'HEAD' });
+		const { contentType, contentSizeMb } = MPHelpers.getFileMetadataByHttpHeaders(metaHeaders);
+
+		if (!supportedMediaTypesConfig.includes(contentType)) {
+			throw new RpcException({
+				message: `${ErrorMessage.CONTENT_TYPE_IS_NOT_SUPPORTED}. File type is: ${contentType}. Supported types: ${supportedMediaTypesConfig.join(', ')}.`,
+				statusCode: HttpStatus.BAD_REQUEST,
+			});
+		}
+
+		if (this.MAX_FILE_SIZE_MB < contentSizeMb) {
+			throw new RpcException({
+				message: `${ErrorMessage.CONTENT_SIZE_IS_TOO_LARGE}. Max content size is ${this.MAX_FILE_SIZE_MB}MB`,
+				statusCode: HttpStatus.BAD_REQUEST,
+			});
+		}
+	}
+
+	private processVideo(filePath: string) {
+		console.log('process video')
+	}
+
+	private processAudio(filePath: string) {
+		console.log('process audio')
+	}
+
+	private processImage(filePath: string) {
+		console.log('process image', filePath);
+	}
+
+	async processFile({ fileUrl } : { fileUrl: string }) {
+		const res = await fetch(fileUrl);
+		const fileType = MPHelpers.getFileType(res.headers.get('content-type'));
+
+		const dirPath = MPHelpers.createUniqueDir(__dirname);
+
+		const { filePath } = await MPHelpers.createFile(res, dirPath);
+		const metadata = await MPHelpers.getFileMetadata(filePath);
+
+		await this.amqpConnection.managedChannel.assertQueue('media:aggregate');
+		await this.amqpConnection.managedChannel.bindQueue('media:aggregate', 'exchange1', 'media:aggregate');
+		this.amqpConnection.publish('exchange1', 'media:aggregate', metadata);
+
+		switch (fileType) {
+			case FileType.IMAGE:
+				this.processImage(filePath);
+				break;
+			case FileType.VIDEO:
+				this.processVideo(filePath);
+				break;
+			case FileType.AUDIO:
+				this.processAudio(filePath);
+		}
 	}
 }
 
