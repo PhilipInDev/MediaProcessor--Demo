2fa32219d1db160135ace3b3823609cfebcf8214
philip <philipatdev@gmail.com>
2023-01-08 16:47:42 +0200

    refactor:error message text

--- microservices/media-processor/src/media-processor/error-message.enum.ts
+++ microservices/media-processor/src/media-processor/error-message.enum.ts
@@ -1,6 +1,6 @@
 enum ErrorMessage {
-	CONTENT_TYPE_IS_NOT_SUPPORTED = 'content-type provided by the link is not supported',
-	CONTENT_SIZE_IS_TOO_LARGE = 'content size is too large',
+	CONTENT_TYPE_IS_NOT_SUPPORTED = 'Content-Type provided by the link is not supported',
+	CONTENT_SIZE_IS_TOO_LARGE = 'Content size is too large',
 }
 
 export { ErrorMessage };
