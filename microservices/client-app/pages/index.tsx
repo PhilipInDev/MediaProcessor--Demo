8179f68af04b1b96bb263388ff875ac0813dc92e
philip <philipatdev@gmail.com>
2023-01-22 18:01:14 +0200

    feat:created media processing page and added it to the root

--- microservices/client-app/pages/index.tsx
+++ microservices/client-app/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
+import MediaProcessingPage from '../components/MediaProcessingPage';
 
-export default function Home() {
+const Home = () => {
   return (
     <>
       <Head>
@@ -9,8 +10,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-      </main>
+      <MediaProcessingPage />
     </>
   )
 }
+
+export default Home;
